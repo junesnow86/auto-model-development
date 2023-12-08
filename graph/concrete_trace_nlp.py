1921ab7641a29eef89faa886bbe5df519f549b18
yileiyang <yyl9510@gmail.com>
2023-12-08 08:45:01 +0000

    better multi process integration final results

--- graph/concrete_trace_nlp.py
+++ graph/concrete_trace_nlp.py
@@ -1,3 +1,4 @@
+from multiprocessing.managers import ListProxy
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -6,7 +7,6 @@ import traceback
 from _collections_abc import MutableMapping
 
 import torch
-from torch.utils._pytree import tree_map
 from transformers import AutoConfig, AutoModel, AutoTokenizer
 from cube.graph.parser.converter import to_fx_graph
 
@@ -15,21 +15,38 @@ import multiprocessing
 from concurrent_log_handler import ConcurrentRotatingFileHandler 
 import logging 
 import time
+import timeout_decorator 
 
-# pip install sentencepiece transformers fuzzywuzzy concurrent-log-handler
+# pip install sentencepiece transformers fuzzywuzzy concurrent-log-handler psutil
 
+@timeout_decorator.timeout(20, timeout_exception=TimeoutError)
+def load_model_with_timeout(config, trust_remote_code):  
+    return AutoModel.from_config(config, trust_remote_code=trust_remote_code) 
+
+text: str = "Huggingface is a really excellent project!"
+
+# different process has different logger, with timestamp
 def setup_logger(log_file):
-    logger = logging.getLogger(f'process-{multiprocessing.current_process().pid}')
+    logger = logging.getLogger(log_file)
     logger.setLevel(logging.DEBUG)
-  
-    handler = ConcurrentRotatingFileHandler(log_file, "a", 1024*1024, 8)
-    formatter = logging.Formatter('%(asctime)s [PID %(process)d][%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')  
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
-    # stream_handler = logging.StreamHandler()
-    # stream_handler.setFormatter(formatter)
-    # logger.addHandler(stream_handler)
+    # logger will only init once for one log_file
+    if not logger.handlers: 
+        handler = ConcurrentRotatingFileHandler(log_file, "a", 1024*1024, 8)
+        formatter = logging.Formatter('%(asctime)s [PID %(process)d][%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')  
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+# different process has the same logger, without timestamp
+def setup_logger2(log_file):
+    logger = logging.getLogger(log_file)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers: 
+        handler = ConcurrentRotatingFileHandler(log_file, "a", 10*1024*1024, 8)
+        logger.addHandler(handler)
 
     return logger
 
@@ -56,66 +73,91 @@ def check_align(before_trace, after_trace):
                 return False
     return True
 
-def trace_single_model(model_name: str, nlp_dir: str, model_name_set: set, tried: list, 
-                       model_loaded: list, success_traced: list,  model_failed: list, error_lock):
+def is_process_alive(pid):
     try:
-        logger = setup_logger(os.path.join(nlp_dir, 'all4debug.log'))
+        proc = psutil.Process(pid)
+        return proc.is_running()
+    except psutil.NoSuchProcess:
+        return False
+
+def get_process_num(process_num_list: ListProxy):
+    for i in range(len(process_num_list)):
+        if process_num_list[i] == multiprocessing.current_process().pid:
+            return i
+        elif not process_num_list[i] or not is_process_alive(process_num_list[i]):
+            process_num_list[i] = multiprocessing.current_process().pid
+            return i
+    raise RuntimeError(f"No available process number, current {process_num_list}")
+
+def trace_single_model(model_name: str, nlp_dir: str, process_num_list: ListProxy, model_name_list: ListProxy): 
+    try:
+        process_num = get_process_num(process_num_list)
+        logger = setup_logger(os.path.join(nlp_dir, f'all4debug_{process_num}.log'))
+
+        model_loaded = False
+        success_traced = False
+        logger_tried = setup_logger2(os.path.join(nlp_dir, 'tried'))
+        logger_loaded = setup_logger2(os.path.join(nlp_dir, 'model_loaded'))
+        logger_traced = setup_logger2(os.path.join(nlp_dir, 'success_traced'))
+        logger_failed = setup_logger2(os.path.join(nlp_dir, 'model_failed'))
+        logger_errors = setup_logger2(os.path.join(nlp_dir, 'errors'))
+
         start_time = time.time()
         # print_memory_usage(f"trying to trace {model_name}")
         logger.info(f"start trying model: {model_name}")
-  
-        logger.info(f"{model_name} traced successfully")
-        tried.append(model_name)
 
-        text: str = "Huggingface is a really excellent project!"
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)  
-        logger.info("Tokenizer loaded")
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+        logger.info(f"{model_name} Tokenizer loaded")
+
         concrete_args = tokenizer(text, return_tensors="pt")
+        logger.info(f"{model_name} tokenized")
         if isinstance(concrete_args, MutableMapping):
             concrete_args = dict(concrete_args)
         config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
-        model = AutoModel.from_config(config, trust_remote_code=True)
-        logger.info("model loaded")  
-        model_loaded.append(f"{model_name}, {config.architectures}")
+        logger.info(f"{model_name} config loaded")
+        model = load_model_with_timeout(config, trust_remote_code=True)
+        model_loaded = True
+        logger.info(f"{model_name} model loaded")  
 
         # model.eval()
         # before_trace = model(**concrete_args)
         
         traced_gm = concrete_trace_wrap(model, concrete_args)
-        logger.info("model traced")  
-        success_traced.append(f"{model_name}, {config.architectures}")
+        success_traced = True
+        logger.info(f"{model_name} model traced")
 
         # traced_gm.eval()
         # after_trace = traced_gm(**concrete_args)
 
         # assert check_align(before_trace, after_trace), "Traced model does not match the original model"
         # forward_aligned.append(f"{model_name}, {config.architectures}")
-        
-        # print_memory_usage("after concrete_trace of " + model_name)
-        # print(f"{model_name} traced successfully")
-    except Exception as e:
-        logger.error(f"fail when trying model: {model_name}", exc_info=True)
-        model_failed.append(f"{model_name}, {config.architectures if 'config' in locals() and config else None}")
+
+    except (Exception, TimeoutError) as e:
+        logger.error(f"fail when trying model: {model_name}", exc_info=False)
+        logger_failed.info(f"{model_name}, {config.architectures if 'config' in locals() and config else None}")
         error_message = traceback.format_exc()
-        logger.error(f"Ready to write errors: {model_name}")
-        with error_lock:
-            with open(os.path.join(nlp_dir, "errors"), "a") as file:
-                file.writelines(f"\n{model_name} failed\n")
-                if 'config' in locals() and config:
-                    file.writelines(f"Architectures: {config.architectures}\n")
-                file.writelines(error_message)
-        logger.error(f"Finish write errors: {model_name}")
-        # print(f"{model_name} failed")
-        # print(error_message)
+
+        logger_errors.error(f"{model_name} failed")
+        if 'config' in locals() and config:
+            logger_errors.error(f"Architectures: {config.architectures}")
+        logger_errors.error(error_message)
+
     finally:
         end_time = time.time()
         logger.info(f"Finish trying model: {model_name}, time: {end_time - start_time:.2f} s")
-        # print(f"Finish trying model: {model_name}, time: {end_time - start_time:.2f} s")
+
+        logger_tried.info(f"{model_name}")
+        if model_loaded:
+            logger_loaded.info(f"{model_name}, {config.architectures}")
+        if success_traced:
+            logger_traced.info(f"{model_name}, {config.architectures}")
+        model_name_list.remove(model_name)
+        logger.info(f"Left models number: {len(model_name_list)}")
 
 if __name__ == "__main__":
     current_file_path = os.path.abspath(__file__)
     current_folder = os.path.dirname(current_file_path)
-    model_name_set_path = os.path.join(current_folder, "huggingface_model_names/model_name_set_nlp") # model_name_set_nlp
+    model_name_set_path = os.path.join(current_folder, "huggingface_model_names/model_name_set_nlp_2")
     with open(model_name_set_path, 'r') as f:
         all_model = eval(f.read())
     print(f"# model: {len(all_model)}")
@@ -137,62 +179,13 @@ if __name__ == "__main__":
     total_memory = mem_info.total
     print(f"Total memory: {total_memory / (1024 ** 3):.2f} GB")
 
-    process_num=23
-    tried_list = []
-    model_loaded_list = []
-    success_traced_list = []
-    model_failed_list = []
+    process_num = multiprocessing.cpu_count() - 1
     with multiprocessing.Manager() as manager:
-        tried = manager.list()
-        model_loaded = manager.list()
-        success_traced = manager.list()
-        # forward_aligned = manager.list()
-        model_failed = manager.list()
+        model_name_list = manager.list(model_name_set)
+        process_num_list = manager.list([0] * process_num)
+        arguments = [(model_name, nlp_dir, process_num_list, model_name_list) for model_name in model_name_set]
 
-        error_lock = manager.Lock()
-
-        arguments = [(model_name, nlp_dir, model_name_set, tried, model_loaded, 
-                    success_traced, model_failed, error_lock) for model_name in model_name_set]
-
-        batch_size = process_num * 4
-        arg_batches = [arguments[i:i + batch_size] for i in range(0, len(arguments), batch_size)]
-
-        for arg_batch in arg_batches:
-            with multiprocessing.Pool(processes = process_num) as pool:
-                results = pool.starmap(trace_single_model, arg_batch)
-                print(results)
-
-                tried_list.extend(list(tried))
-                with open(os.path.join(nlp_dir, "tried"), 'a') as file:
-                    for model_name in list(tried):
-                        file.write(f"{model_name}\n")
-                    tried[:] = []
-                print(f"tried: {len(tried_list)}")
-                    
-                model_loaded_list.extend(list(model_loaded))
-                with open(os.path.join(nlp_dir, "model_loaded"), 'a') as file:
-                    for model_name in list(model_loaded):
-                        file.write(f"{model_name}\n")
-                    model_loaded[:] = []
-                print(f"model_loaded: {len(model_loaded_list)}")
-
-                success_traced_list.extend(list(success_traced))
-                with open(os.path.join(nlp_dir, "success_traced"), 'a') as file:
-                    for model_name in list(success_traced):
-                        file.write(f"{model_name}\n")
-                    success_traced[:] = []
-                print(f"success_traced: {len(success_traced_list)}")
-
-                model_failed_list.extend(list(model_failed))
-                with open(os.path.join(nlp_dir, "model_failed"), 'a') as file:
-                    for model_name in list(model_failed):
-                        file.write(f"{model_name}\n")
-                    model_failed[:] = []
-                print(f"model_failed: {len(model_failed_list)}")
-                # gc.collect()
-
-                print(f"all: {len(model_name_set)}, tried: {len(tried_list)}, \
-                        model_loaded: {len(model_loaded_list)}, success_traced: {len(success_traced_list)}, \
-                        model_failed: {len(model_failed_list)}")
+        with multiprocessing.Pool(processes = process_num) as pool:
+            pool.starmap(trace_single_model, arguments)
 
     print("concrete trace nlp done!")
