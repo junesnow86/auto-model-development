9abcc4a367840215243ad03b1f573c4112b567ca
yileiyang <yyl9510@gmail.com>
2024-01-22 07:51:51 +0000

    concrete trace separate from cube compile

--- graph/concrete_trace_nlp.py
+++ graph/concrete_trace_nlp.py
@@ -1,3 +1,4 @@
+# pip install sentencepiece transformers fuzzywuzzy concurrent-log-handler psutil
 from multiprocessing.managers import ListProxy
 import warnings
 warnings.filterwarnings("ignore")
@@ -14,37 +15,25 @@ import psutil
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 from concurrent_log_handler import ConcurrentRotatingFileHandler 
-import logging 
+import logging
 import time
 import timeout_decorator
-from cube.runtime.utils import microbatches
-import cube
-from examples.utils import get_policy
-import examples.mlp.policy.gallery as gallery
-from functools import partial
-from cube.profiler import CudaTimer
-from cube.profiler.timer import print_each_rank
 
-
-# pip install sentencepiece transformers fuzzywuzzy concurrent-log-handler psutil
 
 text: str = "Huggingface is a really excellent project!"
 cache_dir: str = "/mnt/msrasrg/yileiyang/hf_cache"
 
-cube.init()
-
-# get policy
-policy = get_policy([gallery], "PASMegatronTP")
-policy = partial(policy, nmicros=64//64, tp_size=2)
+torch.set_printoptions(edgeitems=2)
 
 @timeout_decorator.timeout(120, timeout_exception=TimeoutError)
 def load_model_with_timeout(config, trust_remote_code):
+    torch.manual_seed(0)
     return AutoModel.from_config(config, trust_remote_code=trust_remote_code)
 
-def setup_logger(log_file):
+def setup_logger(log_file, level):
     # different process has different logger, with timestamp
     logger = logging.getLogger(log_file)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
     # logger will only init once for one log_file
     if not logger.handlers: 
         handler = ConcurrentRotatingFileHandler(log_file, "a", 1024*1024, 8)
@@ -53,10 +42,10 @@ def setup_logger(log_file):
         logger.addHandler(handler)
     return logger
 
-def setup_logger2(log_file):
+def setup_logger2(log_file, level):
     # different process has the same logger, without timestamp
     logger = logging.getLogger(log_file)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
     if not logger.handlers: 
         handler = ConcurrentRotatingFileHandler(log_file, "a", 10*1024*1024, 8)
         logger.addHandler(handler)
@@ -97,7 +86,7 @@ def concrete_trace_wrap(model, dummy_input):
 def check_align(before_trace, after_trace):
     for key in after_trace.keys():
         if isinstance(after_trace[key], torch.Tensor):
-            if not torch.all(before_trace[key].to(torch.cuda.current_device()) == after_trace[key].to(torch.cuda.current_device())):
+            if not torch.allclose(before_trace[key].to(torch.cuda.current_device()) ,after_trace[key].to(torch.cuda.current_device()), rtol=1e-04, atol=1e-05):
                 return False
     return True
 
@@ -128,15 +117,15 @@ def trace_worker(model_name: str, nlp_dir: str, process_num_list: ListProxy, mod
 def trace_single_model(model_name: str, nlp_dir: str, process_num_list: ListProxy, model_name_list: ListProxy):
     try:
         process_num = get_process_num(process_num_list)
-        logger = setup_logger(os.path.join(nlp_dir, f'all4debug_{process_num}.log'))
+        logger = setup_logger(os.path.join(nlp_dir, f'all4debug_{process_num}.log'), logging.INFO)
 
         model_loaded = False
         success_traced = False
-        logger_tried = setup_logger2(os.path.join(nlp_dir, 'tried'))
-        logger_loaded = setup_logger2(os.path.join(nlp_dir, 'model_loaded'))
-        logger_traced = setup_logger2(os.path.join(nlp_dir, 'success_traced'))
-        logger_failed = setup_logger2(os.path.join(nlp_dir, 'model_failed'))
-        logger_errors = setup_logger2(os.path.join(nlp_dir, 'errors'))
+        logger_tried = setup_logger2(os.path.join(nlp_dir, 'tried'), logging.INFO)
+        logger_loaded = setup_logger2(os.path.join(nlp_dir, 'model_loaded'), logging.INFO)
+        logger_traced = setup_logger2(os.path.join(nlp_dir, 'success_traced'), logging.INFO)
+        logger_failed = setup_logger2(os.path.join(nlp_dir, 'model_failed'), logging.INFO)
+        logger_errors = setup_logger2(os.path.join(nlp_dir, 'errors'), logging.INFO)
 
         start_time = time.time()
         logger.info(f"start trying model: {model_name}")
@@ -149,73 +138,29 @@ def trace_single_model(model_name: str, nlp_dir: str, process_num_list: ListProx
         if isinstance(dummy_input, MutableMapping):
             dummy_input = dict(dummy_input)
 
-        logger.info(dummy_input)
+        logger.debug(f"dummy_input: {dummy_input}")
 
         config = AutoConfig.from_pretrained(model_name, trust_remote_code=True, cache_dir=cache_dir)
         logger.info(f"{model_name} config loaded")
         model = load_model_with_timeout(config, trust_remote_code=True)
         model_loaded = True
         logger.info(f"{model_name} model loaded")
-        logger.info(f"{model_name} has parameter: {sum(p.numel() for p in model.parameters())}")
+        logger.debug(f"{model_name} has parameter: {sum(p.numel() for p in model.parameters())}")
         print_memory_usage(logger, f"after load model {model_name}")
         
         model.eval()
         before_trace = model(**dummy_input)
-        logger.info(f"original logit: {before_trace}")
+        logger.debug(f"original logit: {before_trace['last_hidden_state'][:2]}")
 
         traced_gm = concrete_trace_wrap(model, dummy_input)
         success_traced = True
         logger.info(f"{model_name} model traced")
-
         traced_gm.eval()
         after_trace = traced_gm(**dummy_input)
-        logger.info(f"traced logit: {after_trace}")
+        logger.debug(f"traced logit: {after_trace['last_hidden_state'][:2]}")
 
         assert check_align(before_trace, after_trace), "Traced model does not match the original model"
         logger.info(f"aligned before and after trace: {model_name}, {config.architectures}")
-
-
-        model = load_model_with_timeout(config, trust_remote_code=True)
-        dummy_input = tokenizer(text, return_tensors="pt")
-        import inspect
-        forward_signature = inspect.signature(model.forward)
-        params_with_defaults = [
-            v.default if k != 'input_ids' else dummy_input['input_ids'].to(torch.cuda.current_device())
-            for k, v in forward_signature.parameters.items()
-            if v.default is not inspect.Parameter.empty
-        ]
-        # logger.info(f"forward_signature: {forward_signature}")
-        logger.info(f"params_with_defaults: {params_with_defaults}")
-
-        # compile a training iteration
-        dataloader = microbatches((params_with_defaults, )*4)
-        @cube.compile(model, dataloader, PAS=policy)
-        def train_iter(model, dataloader):
-            logger.info(f"compiled model device: {next(model.model.parameters()).device}")
-            data = next(dataloader)
-            logger.info(f"data: {data}, _full_tensor: {data[0]._full_tensor}")
-            logit = model(*data)
-            logger.info(f"logit: {logit},") #  {logit.device}
-            return logit
-
-        # optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-        # load generated model
-        smodel = cube.utils.load_model()
-        dummy_input = tokenizer(text, return_tensors="pt")
-        import inspect
-        forward_signature = inspect.signature(model.forward)
-        params_with_defaults = [
-            v.default if k != 'input_ids' else dummy_input['input_ids'].to(torch.cuda.current_device())
-            for k, v in forward_signature.parameters.items()
-            if v.default is not inspect.Parameter.empty
-        ]
-        dataloader = microbatches((params_with_defaults, ))
-
-        # run training iteration
-        compiled_logit = train_iter(smodel, dataloader)
-        logger.info(f"compiled logit: {compiled_logit}")
-        assert check_align(before_trace, compiled_logit), "compiled model does not match the original model"
-        logger.info(f"aligned before trace and compiled model: {model_name}, {config.architectures}")
 
     except (Exception, TimeoutError) as e:
         logger.error(f"fail when trying model: {model_name}", exc_info=False)
@@ -227,11 +172,6 @@ def trace_single_model(model_name: str, nlp_dir: str, process_num_list: ListProx
             logger_errors.error(f"Architectures: {config.architectures}")
         logger_errors.error(error_message)
     finally:
-        # CudaTimer().stop('e2e')
-        # print_each_rank('e2e time (ms) per iteration: {} ms'.format(
-        #     CudaTimer().duration(iter_num-warmup, field_name='e2e')))
-        # CudaTimer().print_all(times=iter_num-warmup)
-
         end_time = time.time()
         logger.info(f"Finish trying model: {model_name}, time: {end_time - start_time:.2f} s")
 
@@ -241,13 +181,13 @@ def trace_single_model(model_name: str, nlp_dir: str, process_num_list: ListProx
         if success_traced:
             logger_traced.info(f"{model_name}, {config.architectures}")
         model_name_list.remove(model_name)
-        logger.info(f"Left models number: {len(model_name_list)}")
+        logger.info(f"Left models number: {len(model_name_list)}\n")
 
 
 if __name__ == "__main__":
     current_file_path = os.path.abspath(__file__)
     current_folder = os.path.dirname(current_file_path)
-    model_name_set_path = os.path.join(current_folder, "huggingface_model_names/nlp_test")
+    model_name_set_path = os.path.join(current_folder, "huggingface_model_names/model_name_set_nlp")
     with open(model_name_set_path, 'r') as f:
         all_model = eval(f.read())
     print(f"# model: {len(all_model)}")
@@ -269,19 +209,12 @@ if __name__ == "__main__":
     total_memory = mem_info.total
     print(f"Total memory: {total_memory / (1024 ** 3):.2f} GB")
 
-    # process_num = 1
-    # with multiprocessing.Manager() as manager:
-    #     model_name_list = manager.list(model_name_set)
+    process_num = multiprocessing.cpu_count() - 20
+    with multiprocessing.Manager() as manager:
+        model_name_list = manager.list(model_name_set)
         
-    #     process_num_list = manager.list([0] * process_num)
-    #     arguments = [(model_name, nlp_dir, process_num_list, model_name_list) for model_name in model_name_set]
-    #     with ThreadPool(processes = process_num) as pool:
-    #         pool.starmap(trace_worker, arguments)
+        process_num_list = manager.list([0] * process_num)
+        arguments = [(model_name, nlp_dir, process_num_list, model_name_list) for model_name in model_name_set]
+        with ThreadPool(processes = process_num) as pool:
+            pool.starmap(trace_worker, arguments)
 
-    # with multiprocessing.Manager() as manager:
-    #     model_name_list = manager.list(model_name_set)
-    #     process_num_list = manager.list([0] * 1)
-    for model_name in model_name_set:
-        trace_single_model(model_name, nlp_dir, [0], list(model_name_set))
-
-    print("concrete trace nlp done!")
